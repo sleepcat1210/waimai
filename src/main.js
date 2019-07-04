72b162730b58beb4e4c0463c8f8be1f948d8d1e5
sleepcat1210 <18224513696@163.com>
2019-07-04 13:57:21 +0800

    '优化'

--- src/main.js
+++ src/main.js
@@ -6,8 +6,16 @@ import App from './App'
 import router from './router'
 import store from './store'
 import {Button} from 'mint-ui'
+import './mock/mockServer.js'
+import VueLazyload from 'vue-lazyload'
+import loading from './common/imgs/loading.gif'
+import Split from './components/Split/Split.vue'
+import './filters/index.js' // 加载过滤器
 Vue.config.devtools = true
-Vue.component(Button.name,Button)
+Vue.component(Button.name,Button,Split)
+Vue.use(VueLazyload, { // 内部自定义一个指令lazy
+	loading
+  })
 new Vue({
 	el: '#app',
 	render: h=> h(App),
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,10 +1,26 @@
-import {reqAddress,reqCategorys,reqShops, reqLogOut} from '../api'
+import {
+  reqAddress,
+  reqCategorys,
+  reqShops,
+   reqLogOut,
+   reqShopRatings,
+  reqShopGoods,
+  reqShopInfo,  
+  reqSearchShop
+  } from '../api'
 import {
     RECEIVE_ADDRESS,
     RECEIVE_CATEGORYS,
     RECEIVE_SHOPS,
      RESET_USER_INFO,
-     RECEIVE_USER_INFO
+     RECEIVE_USER_INFO,
+     RECEIVE_GOODS,
+  RECEIVE_RATINGS,
+  RECEIVE_INFO,
+  INCREMENT_FOOD_COUNT,
+  DECREMENT_FOOD_COUNT,
+  CLEAR_CART,
+  RECEIVE_SEARCH_SHOPS
     } from './mutation-types'
 export default{
     //异步获取地址
@@ -40,12 +56,65 @@ export default{
       commit(RECEIVE_USER_INFO, {userInfo})
     }
   },
-
     //异步退出
     async reqLogOut({commit}){
         const result =await reqLogOut()
         if(result.code===0){
             commit(RESET_USER_INFO)
         }
+    },
+    // 异步获取商家信息
+  async getShopInfo({commit}) {
+    const result = await reqShopInfo()
+    if (result.code === 0) {
+      const info = result.data
+      commit(RECEIVE_INFO, {info})
     }
+  },
+
+  // 异步获取商家评价列表
+  async getShopRatings({commit}, callback) {
+    const result = await reqShopRatings()
+    if (result.code === 0) {
+      const ratings = result.data
+      commit(RECEIVE_RATINGS, {ratings})
+      // 数据更新了, 通知一下组件
+      callback && callback()
+    }
+  },
+
+  // 异步获取商家商品列表
+  async getShopGoods({commit}, callback) {
+    const result = await reqShopGoods()
+    if (result.code === 0) {
+      const goods = result.data
+      commit(RECEIVE_GOODS, {goods})
+      // 数据更新了, 通知一下组件
+      callback && callback()
+    }
+  },
+   // 同步更新food中的count值
+   updateFoodCount({commit}, {isAdd, food}) {
+    if (isAdd) {
+      commit(INCREMENT_FOOD_COUNT, {food})
+    } else {
+      commit(DECREMENT_FOOD_COUNT, {food})
+    }
+  },
+
+  // 同步清空购物车
+  clearCart({commit}) {
+    commit(CLEAR_CART)
+  },
+
+  // 异步获取商家商品列表
+  async searchShops({commit, state}, keyword) {
+
+    const geohash = state.latitude + ',' + state.longitude
+    const result = await reqSearchShop(geohash, keyword)
+    if (result.code === 0) {
+      const searchShops = result.data
+      commit(RECEIVE_SEARCH_SHOPS, {searchShops})
+    }
+  },
 }
